833758ce468fafa6a6fe57e90bfde2a747fca4b9
Obie Zuriel <obiezuriel631@gmail.com>
2025-11-30 13:55:19 +0700

    product_not_done

---
+++ resources/views/components/product-card.blade.php
@@ -0,0 +1,11 @@
+<form method="POST" class="flex flex-col items-center justify-center bg-[#F4F5FF] rounded-lg p-4">
+    <img src="" alt="">
+    <div>
+        <h3>{{ $productName }}</h3>
+        <div class="flex">
+            <img src="/images/misc/star.svg" alt="star">
+            <p>{{ $rating }}</p>
+        </div>
+
+    </div>
+</form>
